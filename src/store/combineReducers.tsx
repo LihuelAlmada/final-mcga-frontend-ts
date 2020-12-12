47c5ca3f491c53286a88d906af8db7cc5ae6e733
Necralma <lihuel.almada@gmail.com>
2020-12-12 19:21:22 -0300

    Updated combineReducer

--- src/store/combineReducers.tsx
+++ src/store/combineReducers.tsx
@@ -2,4 +2,4 @@ import {combineReducers} from 'redux';
 import { userReducer} from './user/reducers';
 import { noteReducer} from './note/reducers';
 
-export const reducer = combineReducers({user: userReducer, note: noteReducer});
+export default combineReducers({user: userReducer, note: noteReducer});
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,6 +1,22 @@
-import {createStore, applyMiddleware} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
-import {reducer} from './combineReducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './combineReducers';
 
-export const  store  =  createStore ( reducer ,  composeWithDevTools (applyMiddleware())); //dentro de la funcion tendria que ir "...middleware" pero no me lo tomo
-    
+const initialState = {};
+
+const middleWare = [thunk];
+
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(...middleWare))
+);
+
+export default store;
