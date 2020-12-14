44426636ce825146379c97639be9b052ada4bce4
Necralma <lihuel.almada@gmail.com>
2020-12-14 20:45:28 -0300

    delete some coments

--- src/components/screens/NoteForm/NoteForm.tsx
+++ src/components/screens/NoteForm/NoteForm.tsx
@@ -1,20 +1,22 @@
 import './styleform.css';
 import React, { useState} from 'react';
-//import { Formik, Form, Field } from "formik";
-import {INoteReduxProps, ITarget, INoteAdd, INote } from '../../../interfaces';
+import {INoteReduxProps, ITarget, INoteAdd } from '../../../interfaces';
 import {addNote} from '../../../store/note/actions';
 import { connect } from 'react-redux'
 import { Form, Field } from 'react-final-form';
 
 const NoteForm = ({addNote }: INoteAdd) => {
- 
+
   const [title, setTitle] = useState('');
   const [description,setDescription] = useState('');
-  //const [updatedAt, setDate] = useState(Date);
 
-  const handleChangeTitle = (e: ITarget) => setTitle(e.target.value);
+  const handleChangeTitle = (e: ITarget) =>{setTitle(e.target.value)} 
+
   const handleChangeDescription = (e: ITarget) => setDescription(e.target.value);
 
+  /*onChange = e =>{
+    this.setStage({ [e.target.name]: e.target.value})
+  }*/
   const handleOnSubmit = (e: any) => {
       e.preventDefault();
       const newNote = {
@@ -34,11 +36,11 @@ const NoteForm = ({addNote }: INoteAdd) => {
         <h2>Simple Default Input</h2>
         <div>
           <label>Title</label>
-          <Field name="title" component="input" placeholder="title" onChange={handleChangeTitle}/>
+          <Field name="title" component="input" placeholder={title} onChange={handleChangeTitle} />
         </div>
         <div>
           <label>Description</label>
-          <Field name="description" component="input" placeholder="Description" onChange={handleChangeDescription}/>
+          <Field name="description" component="input" placeholder={description} onChange={handleChangeDescription} />
         </div>
         <button type="submit">Add</button>
       </form>
@@ -52,32 +54,3 @@ const mapStateToProps = (state: INoteReduxProps) => ({
 });
 
 export default connect(mapStateToProps, { addNote })(NoteForm);
-
-/* <Formik
-          initialValues={{ title: "", description: "", updateupdatedAt: "" }}
-          onSubmit={(initialValues: any) => { console.log("formik");
-        }}
-      >
-        <Form onSubmit={handleOnSubmit}>
-          <div className="containerAddNote">
-            <h4> Add Notes</h4>
-            <Field
-              type="text"
-              className="titleNote"
-              name="title"
-              placeholder="Title"
-              onChange={handleChangeTitle}
-            />
-            <Field
-              type="text"
-              className="descriptionNote"
-              name="description"
-              placeholder="Description"
-              onChange={handleChangeDescription}
-            />
-            <button className="btnAddNote" type="submit" >
-              Add
-            </button>
-          </div>
-        </Form>
-      </Formik>*/
