3125acf6c867804757cd7ffbaa13a06d58c9fbfb
MatiasForconi <matiasfernandoforconi@hotmail.com>
2020-12-15 20:59:03 -0300

    updated structure html and added css in AppNavBar

--- src/components/screens/AppNavBar/AppNavBar.tsx
+++ src/components/screens/AppNavBar/AppNavBar.tsx
@@ -1,22 +1,29 @@
+import './app.css';
 import React, { Component, Fragment, useState } from 'react';
-import { Form, Field } from 'react-final-form';
+//import { Form, Field } from 'react-final-form';
 import {Link} from 'react-router-dom';
 
 const AppNavbar = () => {
     return(
-        <div className="navbar">
-            Altas Notas
-            <button type="submit">submit</button>
-            <Link to = "/login">
-                <button className="button">Login</button>
-            </Link>
-            <Link to = "/register">
-                <button className="button">Register</button>
-            </Link>
-            <Link to = "/home">
-                <button className="button">Home</button>
-            </Link>
-        </div>
+        <div className="wrapper">
+            <div className="header">
+                <h1>Altas Notas</h1>
+            </div>
+            <div className="navbar">
+                <Link to = "/login">
+                    <button className="button">Login</button>
+                </Link>
+                <Link to = "/register">
+                    <button className="button">Register</button>
+                </Link>
+                <Link to = "/home">
+                    <button className="button">Home</button>
+                </Link>
+                <div>
+                <button type="submit">submit</button>
+                </div>
+            </div>
+        </div>    
     );
 }
 
--- src/components/screens/SignIn/SignIn.tsx
+++ src/components/screens/SignIn/SignIn.tsx
@@ -1,4 +1,4 @@
-import './style.css';
+import './signin.css';
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {Form, Field} from 'react-final-form'
